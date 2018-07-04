408170bb9d1e01e5c853394f7ed6b8c9895326dc
Stefan Birkner <mail@stefan-birkner.de>
2018-07-04 10:50:40 +0200

    Add proper test for RdcTestResultWatcher

--- src/test/java/com/saucelabs/rdc/RdcTestResultWatcherTest.java
+++ src/test/java/com/saucelabs/rdc/RdcTestResultWatcherTest.java
@@ -1,53 +1,217 @@
 package com.saucelabs.rdc;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Rule;
-import org.junit.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
-@Ignore
+import com.github.tomakehurst.wiremock.stubbing.ServeEvent;
+import org.junit.*;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+import org.junit.internal.AssumptionViolatedException;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.SessionId;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.saucelabs.rdc.RdcCapabilities.API_KEY;
+import static java.util.UUID.randomUUID;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@RunWith(Parameterized.class)
 public class RdcTestResultWatcherTest {
 
+	@Parameters(name = "{0}")
+	public static Object[][] data() {
+		return new Object[][] {
+			{
+				"successful test",
+				(Runnable) () -> {},
+				true
+			},
+			{
+				"failing test",
+				(Runnable) () -> fail("expected"),
+				false
+			},
+			{
+				"errored test",
+				(Runnable) () -> { throw new RuntimeException("expected"); },
+				false
+			},
+			{
+				"test that fails an assumption",
+				(Runnable) () -> assumeTrue("expected", false),
+				false
+			},
+			{
+				"test that throws deprecated AssumptionViolatedException",
+				(Runnable) () -> {
+					throw new AssumptionViolatedException("expected");
+				},
+				false
+			},
+		};
+	}
+
+	@Parameter(0)
+	public String name; //Parameterized requires a field for each parameter
+
+	@Parameter(1)
+	public Runnable test;
+
+	@Parameter(2)
+	public boolean expectedToPass;
+
 	@Rule
-	public RdcTestResultWatcher watcher = new RdcTestResultWatcher();
-	private AppiumDriver driver;
+	public final WireMockRule wireMockRule
+		= new WireMockRule(options().dynamicPort());
 
-	@Before
-	public void setup() {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
-		capabilities.setCapability(RdcCapabilities.API_KEY, "Your project API key goes here");
+	@Rule
+	public final EnvironmentVariables environmentVariables
+		= new EnvironmentVariables();
 
-		driver = new AndroidDriver(RdcEndpoints.EU_ENDPOINT, capabilities);
-		watcher.setRemoteWebDriver(driver);
+	@Rule
+	public final SystemOutRule systemOut
+		= new SystemOutRule().enableLog().mute();
 
-		printUsefulLinks();
+	@Test
+	public void isReportedAsPassed() {
+		assumeTrue(expectedToPass);
+
+		TestClass.setWebDriver = true;
+		serverSendsResponse(204);
+		webDriverHasArbitrarySessionId();
+
+		runTest();
+
+		assertEquals("{\"passed\":true}", bodyOfRequest());
 	}
 
 	@Test
-	public void updateTestResultsOnSauceLabsRdcAfterCalculatorTest() {
-		MobileElement buttonTwo = (MobileElement) driver.findElementById("net.ludeke.calculator:id/digit2");
-		MobileElement buttonPlus = (MobileElement) driver.findElementById("net.ludeke.calculator:id/plus");
-		MobileElement buttonEquals = (MobileElement) driver.findElementById("net.ludeke.calculator:id/equal");
-		MobileElement resultField = (MobileElement) driver.findElementByXPath("//android.widget.EditText[1]");
+	public void isNotReportedAsPassed() {
+		assumeFalse(expectedToPass);
 
-		buttonTwo.click();
-		buttonPlus.click();
-		buttonTwo.click();
-		buttonEquals.click();
+		TestClass.setWebDriver = true;
+		serverSendsResponse(204);
+		webDriverHasArbitrarySessionId();
 
-		WebDriverWait wait = new WebDriverWait(driver, 30);
-		wait.until(ExpectedConditions.textToBePresentInElement(resultField, "4"));
+		runTest();
+
+		assertEquals("{\"passed\":false}", bodyOfRequest());
 	}
 
-	private void printUsefulLinks() {
-		System.out.println("Live view: " + driver.getCapabilities().getCapability(RdcCapabilities.TEST_LIVE_VIEW_URL));
-		System.out.println("Test report: " + driver.getCapabilities().getCapability(RdcCapabilities.TEST_REPORT_URL));
+	@Test
+	public void isReportedToSessionsTestEndpoint() {
+		TestClass.setWebDriver = true;
+		serverSendsResponse(204);
+		SessionId sessionId = randomSessionId();
+		webDriverHasSessionId(sessionId);
+
+		runTest();
+
+		wireMockRule.verify(
+			putRequestedFor(
+				urlEqualTo("/rest/v2/appium/session/" + sessionId + "/test")));
 	}
 
+	@Test
+	public void isReportedAsJsonDocument() {
+		TestClass.setWebDriver = true;
+		serverSendsResponse(204);
+		webDriverHasArbitrarySessionId();
+
+		runTest();
+
+		wireMockRule.verify(
+			putRequestedFor(anyUrl())
+				.withHeader("Accept", equalTo("application/json")));
+	}
+
+	@Test
+	public void writesErrorWhenServerReturnsWrongStatus() {
+		TestClass.setWebDriver = true;
+		serverSendsResponse(500);
+		webDriverHasArbitrarySessionId();
+
+		runTest();
+
+		assertEquals(
+			"Test report status might not be updated on Sauce Labs RDC (TestObject). Status: 500\n",
+			systemOut.getLogWithNormalizedLineSeparator());
+	}
+
+	private void serverSendsResponse(int status) {
+		wireMockRule.stubFor(
+			put(anyUrl()).willReturn(aResponse().withStatus(status)));
+	}
+
+	private void webDriverHasArbitrarySessionId() {
+		when(TestClass.webDriver.getSessionId())
+			.thenReturn(randomSessionId());
+	}
+
+	private void webDriverHasSessionId(SessionId id) {
+		when(TestClass.webDriver.getSessionId())
+			.thenReturn(id);
+	}
+
+	private SessionId randomSessionId() {
+		return new SessionId(randomUUID().toString());
+	}
+
+
+	private Result runTest() {
+		environmentVariables.set(
+			"API_URL", "http://127.0.0.1:" + wireMockRule.port());
+		TestClass.test = test;
+		when(TestClass.webDriver.getCapabilities())
+			.thenReturn(capabilitiesWithApiKey("dummy-api-key"));
+		return JUnitCore.runClasses(TestClass.class);
+	}
+
+	private Capabilities capabilitiesWithApiKey(String apiKey) {
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(API_KEY, apiKey);
+		return capabilities;
+	}
+
+	private String bodyOfRequest() {
+		ServeEvent serveEvent = wireMockRule.getAllServeEvents().get(0);
+		return serveEvent.getRequest().getBodyAsString();
+	}
+
+	public static class TestClass {
+
+		@Rule
+		public RdcTestResultWatcher watcher = new RdcTestResultWatcher();
+
+		static boolean setWebDriver = false;
+		static RemoteWebDriver webDriver = mock(RemoteWebDriver.class);
+
+		static Runnable test; //used to inject the test itself
+
+		@Before
+		public void setup() {
+			if (setWebDriver) {
+				watcher.setRemoteWebDriver(webDriver);
+			}
+		}
+
+		@Test
+		public void test() {
+			test.run();
+		}
+	}
 }
