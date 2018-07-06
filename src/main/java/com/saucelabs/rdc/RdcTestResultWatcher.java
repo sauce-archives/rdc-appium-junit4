740f0573213c01b5d88c1fea0478de6cf69dd4c7
Stefan Birkner <mail@stefan-birkner.de>
2018-07-06 09:48:53 +0200

    Tidy up RdcTestResultWatcher

--- src/main/java/com/saucelabs/rdc/RdcTestResultWatcher.java
+++ src/main/java/com/saucelabs/rdc/RdcTestResultWatcher.java
@@ -1,6 +1,5 @@
 package com.saucelabs.rdc;
 
-import com.saucelabs.rdc.helper.RdcEnvironmentVariables;
 import com.saucelabs.rdc.helper.RdcListenerProvider;
 import com.saucelabs.rdc.helper.reporter.ResultReporter;
 import org.junit.rules.TestRule;
@@ -12,39 +11,36 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import static com.saucelabs.rdc.RdcEndpoints.DEFAULT_API_ENDPOINT;
+import static com.saucelabs.rdc.helper.RdcEnvironmentVariables.getApiEndpoint;
 
 public class RdcTestResultWatcher implements TestRule {
 
-	private ResultReporter reporter;
+	private RemoteWebDriver webDriver;
 
-	private RdcListenerProvider provider;
-
-	public RdcTestResultWatcher() {
-		provider = RdcListenerProvider.newInstance();
+	public void setRemoteWebDriver(RemoteWebDriver webDriver) {
+		this.webDriver = webDriver;
 	}
 
 	@Override
-	public Statement apply(final Statement base, final Description description) {
+	public Statement apply(Statement base, Description description) {
 		return new Statement() {
 			@Override
 			public void evaluate() throws Throwable {
 				try {
 					base.evaluate();
-					reportStatus(true);
+					safeReportStatus(true);
 				} catch (Throwable e) {
-					reportStatus(false);
+					safeReportStatus(false);
 					throw e;
-				} finally {
-					closeReporter();
 				}
 			}
 		};
 	}
 
-	private void reportStatus(boolean status) {
-		if (reporter != null) {
+	private void safeReportStatus(boolean status) {
+		if (webDriver != null) {
 			try {
-				reporter.createSuiteReportAndTestReport(status);
+				reportStatus(status);
 			} catch (Exception e) {
 				System.err.println(
 					"Failed to update test report. Caused by "
@@ -53,25 +49,17 @@ public class RdcTestResultWatcher implements TestRule {
 		}
 	}
 
-	private void closeReporter() {
-		if (reporter != null) {
-			reporter.close();
-		}
+	private void reportStatus(boolean status) throws MalformedURLException {
+		RdcListenerProvider provider = RdcListenerProvider.newInstance();
+		provider.setApiUrl(apiUrl());
+		provider.setDriver(webDriver);
+
+		ResultReporter reporter = new ResultReporter(provider);
+		reporter.createSuiteReportAndTestReport(status);
+		reporter.close();
 	}
 
-	public void setRemoteWebDriver(RemoteWebDriver driver) {
-		setApiUrl();
-		provider.setDriver(driver);
-		reporter = new ResultReporter(provider);
-	}
-
-	private void setApiUrl() {
-		URL apiUrl;
-		try {
-			apiUrl = new URL(RdcEnvironmentVariables.getApiEndpoint().orElse(DEFAULT_API_ENDPOINT));
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e.getMessage(), e);
-		}
-		provider.setApiUrl(apiUrl);
+	private URL apiUrl() throws MalformedURLException {
+		return new URL(getApiEndpoint().orElse(DEFAULT_API_ENDPOINT));
 	}
 }
